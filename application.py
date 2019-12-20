4b8baaba54b0bf155a6631c7407bf60e506e9885
jackhsien2002 <jackhsien2002@gmail.com>
2019-12-20 12:33:21 +0800

    refactor application.py

--- application.py
+++ application.py
@@ -93,23 +93,31 @@ def addBook():
 
 def findBook(url, q):
 	'''
-	given a goodread search API and a query string, return a list of book dictionary.
-	with book dictionary, we can get value with keys like book id, title, author and average rating
+	given a goodread url as string and a q as query string
+	return a list of book dictionary. every book in the list has information of id, title, author and average rating
+
 	When flask backend send a request to goodread, it responds with xml objects. 
 	Here, we use element tree, which is like dictionary, to parse the xml. 
-	Every tags in xml corresponds to a node in the element tree, and we can retrieve the value of the tags by using key of the node.
+	Every tag in xml corresponds to a node in the element tree, and we can retrieve the value of the tags by using key of the node.
 	'''
 	response = requests.get(url, params = {"key" : KEY, "q" : q})
 	root = ET.fromstring(response.content)
+	#all books that match search result locate at index-1 at level-1 and index-6 at level-2
 	result = root[1][6]
 	books = [];
 	try:
+		#retrieve detail of books
 		for book in result:
 			book_id_api = book[8][0].text
 			book_title = book[8][1].text
 			author = book[8][2][1].text
 			average_rating = book[7].text
-			books.append({"book_id_api" : book_id_api, "title" : book_title, "author" : author, "average_rating" : average_rating})
+			books.append({
+				"book_id_api" : book_id_api,
+				"title" : book_title, 
+				"author" : author, 
+				"average_rating" : average_rating
+			})
 		return books
 	except:
 		books = None
@@ -117,65 +125,71 @@ def findBook(url, q):
 
 @celery.task(name= 'application.write_book_to_database')
 def write_book_to_database(book_info):
+	sql_command = "INSERT INTO books (isbn, title, author, year, id_api, work_rating_count, average_rating)\
+					 VALUES (:isbn, :title, :author, :year, :id_api, :work_rating_count, :average_rating)"
 	db.execute(
-        "INSERT INTO books (isbn, title, author, year, id_api, work_rating_count, average_rating) VALUES (:isbn, :title, :author, :year, :id_api, :work_rating_count, :average_rating)",
-        book_info)
+		sql_command,
+        book_info
+	)
 	db.commit()
 
 @app.route("/book_detail/<int:id_api>", methods = ["GET", "POST"])
 def bookDetail(id_api):
 	'''
-	given a book id, send a request to goodread
+	given a book id as integer, send a request to goodread
 	'''
+	#query book from database according to book's id_api
 	book = db.execute("SELECT * FROM books WHERE id_api=:id_api", {"id_api" : id_api}).fetchone()
 
-	#if book does not have id in data base
+	#if book does not exist in database
 	if book is None:
-		#update lacking data to data base
-		#store column (isbn, title, author, year, id_api, work_rating_count, average_rating) 
+		#update data to data base
+		##store column (isbn, title, author, year, id_api, work_rating_count, average_rating) 
 		url = "https://www.goodreads.com/book/show.xml"
 		response = requests.get(url, params = {"key" : KEY, "id" : id_api})
+		print (response)
 
 		root = ET.fromstring(response.content)
 		book_node = root.find("book")
-		book_details = parseDetailFromBookNode(book_node, id_api)
+		book = parseDetailFromBookNode(book_node, id_api)
+		#invoke celery to work on writing book details to database
+		write_book_to_database.delay(book)
 
-		write_book_to_database.delay(book_details)
-
+	#if user submit a review for the book
 	if request.method == "POST":
-
-		user_id = session["user_id"]
-
+		#if user does not leave review before
+		user_id = session["user_id"]		
 		if not isUserWriteReview(id_api, user_id):	
-
+			#retrieve user input
 			review = request.form.get("review")
 			rating =request.form.get("rating")
-
+			#update database with user's review on the book
 			sql_command = "INSERT INTO reviews (review, rating, book_id, user_id) \
 							VALUES (:review, :rating, :book_id, :user_id)"
-
 			sql_parameters = {
 				"review" : review,
 				"rating" : rating,
-				"book_id" : int(id_api),
+				"book_id" : id_api,
 				"user_id" : user_id
 			}
-
 			db.execute(sql_command, sql_parameters)
 			db.commit()
-			mean_review_rating = getMeanReviewRating(int(id_api))
-			book = updateMeanRating(int(id_api), mean_review_rating)
-			
-	users_review = getBookReview(int(id_api))
+			#calcualte rating of the book right after user submit rating
+			mean_review_rating = getMeanReviewRating(id_api)
+			#update the rating to databse and get the newest book object from database
+			book = updateMeanRating(id_api, mean_review_rating)
+
+	#retrieve all the reviews on a given book
+	users_in_reviews = getBookReview(id_api)
 
 	return render_template("book_detail.html", 
 							book = book, 
 							username = session["username"], 
-							users_review = users_review)
+							users_in_reviews = users_in_reviews)
 
 def parseDetailFromBookNode(book_node, id_api):
 	'''
-	given a book node, parse book detail from its node
+	given a book node and an id_api as integer, parse book detail from tree nodes
 	return a book dictionary
 	'''
 	isbn = book_node.find("isbn").text
@@ -194,28 +208,32 @@ def parseDetailFromBookNode(book_node, id_api):
 		"title" : title, 
 		"author" : author,
 		"year" : year, 
-		"id_api" : int(id_api),
+		"id_api" : id_api,
 		"work_rating_count" : int(work_rating_count), 
 		"average_rating" : float(average_rating),
 		"mean_review_rating" : None
 	}
 
 def getBookReview(book_id):
-	'''given book id, retrieve its user review, rating
+	'''
+	given book id as integer, retrieve users in the book reviews.
 	'''
 	#argument must be integer
 	assert isinstance(book_id, int)
 
-	#prepare sql command for alchemy
+	#prepare sql command for SQLAlchemy
 	sql_command = "SELECT * FROM users JOIN reviews ON reviews.user_id=users.id WHERE book_id=:book_id"
 	sql_parameters = {"book_id" : book_id}
 
-	#query data from database by sql command
-	users = db.execute(sql_command, sql_parameters).fetchall()
+	#query all uses' reviews from database
+	users_in_reviews = db.execute(sql_command, sql_parameters).fetchall()
 
-	return users
+	return users_in_reviews
 
 def isUserWriteReview(book_id, user_id):
+	'''
+	if user write book review before, return True; If not, return False
+	'''
 	sql_command = "SELECT * FROM reviews WHERE book_id=:book_id AND user_id=:user_id"
 	sql_parameters = {"book_id" : book_id, "user_id" : user_id}
 	review = db.execute(sql_command, sql_parameters).fetchone()
@@ -225,6 +243,10 @@ def isUserWriteReview(book_id, user_id):
 		return True
 
 def getMeanReviewRating(book_id):
+	'''
+	given a book id as integer, average all the ratings of a book
+	return average rating of book
+	'''
 	sql_command = "SELECT rating FROM reviews WHERE book_id=:book_id"
 	sql_parameters = {"book_id" : book_id}
 	reviews = db.execute(sql_command, sql_parameters).fetchall()
@@ -236,9 +258,15 @@ def getMeanReviewRating(book_id):
 	return total/count
 
 def updateMeanRating(book_id, mean_review_rating):
+	'''
+	given book_id and mean_review_rating as integer, update book object in database
+	return book object that is updated
+	'''
 	sql_command = "UPDATE books SET mean_review_rating=:mean_review_rating WHERE id_api=:book_id"
-	sql_parameters = {"mean_review_rating" : mean_review_rating,
-					  "book_id" : book_id}
+	sql_parameters = {
+		"mean_review_rating" : mean_review_rating,
+		"book_id" : book_id
+	}
 	db.execute(sql_command, sql_parameters)
 	db.commit()
 
@@ -250,6 +278,10 @@ def updateMeanRating(book_id, mean_review_rating):
 
 @app.route("/login", methods = ["POST", "GET"])
 def loginPage():
+	'''
+	at loginPage view, user will be prompted with username and password field.
+	after user type valid user information, they will login to application
+	'''
 	if request.method == "GET":	
 		return render_template("login.html")
 	elif session.get("username", None) != None:
@@ -267,6 +299,10 @@ def loginPage():
 			return render_template("login.html", error_message = error_message)
 
 def getUserId(username):
+	'''
+	given username as string, query user information from database
+	return user id
+	'''
 	sql_command = "SELECT * FROM users WHERE username=:username"
 	sql_parameters = {"username" : username}
 	user = db.execute(sql_command, sql_parameters).fetchone()
@@ -274,6 +310,10 @@ def getUserId(username):
 
 
 def isUserExist(username, password):
+	'''
+	given username and password as string
+	return True if user data is in database; else return False
+	'''
 	person = db.execute("SELECT * FROM users WHERE username=:username AND password=:password",
 				{"username" : username, "password" : password}).fetchall()
 	error_message = ""
@@ -288,17 +328,28 @@ def isUserExist(username, password):
 
 @app.route("/logout", methods = ["GET"])
 def logout():
+	'''
+	logout the user by setting user's session as None at our database
+	'''
 	session["username"] = None
 	session["user_id"] = None
 	return render_template("logout.html")
 
 
 @app.route("/signup", methods=["GET"])
-def signUp():	
+def signUp():
+	'''
+	user signup here with username and password
+	'''
 	return render_template("sign.html")
 
 @app.route("/create", methods=["POST"])
 def create():
+	'''
+	extract user's information from their signup form and create user's data at our database
+	if user's information is written to database successfully,
+	return create_success page, else return create_fail page
+	'''
 	#retrieve form username and password from request
 	username = request.form.get("username")
 	password = request.form.get("password")
@@ -314,6 +365,10 @@ def create():
 
 @app.route("/api/<string:isbn>", methods=["GET"])
 def getAPI(isbn):
+	'''
+	given isbn as string, query book from our database and return it to user in JSON format
+	if book does not exist in our database, return an error page 
+	'''
 	sql_command = "SELECT * FROM books WHERE isbn=:isbn"
 	sql_parameters = {"isbn" : isbn}
 	book = db.execute(sql_command, sql_parameters).fetchone()
